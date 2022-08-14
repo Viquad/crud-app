e34eb8b23efed5446d3700e4feded5246bfcc3d9
Viquad <45936059+Viquad@users.noreply.github.com>
2022-08-14 16:58:17 +0300

    add goose based schema

--- scheme/accounts.sql
+++
@@ -1,8 +0,0 @@
-create table accounts (
-    id serial unique not null,
-    first_name varchar(255) not null,
-    last_name varchar(255) not null,
-    balance int default 0 not null,
-    currency varchar(10) not null,
-    last_update timestamp default now()
-);
