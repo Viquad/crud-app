8b8a539d666709cdb3113d5b05d37fbf34ab989d
Viquad <45936059+Viquad@users.noreply.github.com>
2022-08-25 22:11:01 +0300

    fix sql schemas

--- schema/20220822124343_create_accounts.up.sql
+++ schema/20220822124343_create_accounts.up.sql
@@ -1,10 +1,7 @@
 CREATE TABLE IF NOT EXISTS accounts (
-    id SERIAL UNIQUE,
-    user_id INT UNIQUE,
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES users(id),
     balance INT DEFAULT 0 NOT NULL,
     currency VARCHAR(10) NOT NULL,
-    last_update TIMESTAMP DEFAULT NOW(),
-    PRIMARY KEY (id),
-    FOREIGN KEY (user_id)
-        REFERENCES users(id)
+    last_update TIMESTAMP DEFAULT NOW()        
 );
